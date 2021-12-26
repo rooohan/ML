565a0d48b9697c1db8062b3676b298da755cd189
rooohan <roohan.lee@gmail.com>
2021-12-26 20:09:41 +0800

    完成归一化的两种算法

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import operator
+import pandas as pd
 
 
 def createDataSet():
@@ -17,17 +18,17 @@ def classify0(inX, dataSet, labels, k):
     :param k: 选取前k个临近的
     :return:
     """
-    assert len(inX)==dataSet.shape[1]
-    assert k<=dataSet.shape[0]
+    assert len(inX) == dataSet.shape[1]
+    assert k <= dataSet.shape[0]
     dataSetSize = dataSet.shape[0]
     # 将inX复制dataSet集合的个数;np.tile(inX, )如果是一个int，就是将inX的属性复制int次
     # 如果是（x，y）就是将inX的属性复制y次，行复制x次,所以下面代码的意思是让inX的长度与dataSet一致
-    align_inX = np.tile(inX, (dataSetSize,1))
+    align_inX = np.tile(inX, (dataSetSize, 1))
     # 分别做差
     diffMat = align_inX - dataSet
     # 各自平方
     sqDiffMat = diffMat ** 2
-    #将属性的平方和相加
+    # 将属性的平方和相加
     sqDistances = sqDiffMat.sum(axis=1)
     # 开根号
     distances = sqDistances ** 0.5
@@ -41,7 +42,27 @@ def classify0(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
-if __name__ == "__main__":
-    group, labels = createDataSet()
-    results = classify0([0,1], group, labels,2)
+def load_data(url):
+    base_data = pd.read_csv(url, delim_whitespace=True, header=None)
+    arr_x = base_data.loc[:, 0:2].values
+    arr_y = base_data.loc[:, 3].values
+    arr_y = arr_y.reshape((arr_y.shape[0], 1))
+    return arr_x, arr_y
+
+def classify1(targe_vector, k, x, y):
+    assert len(targe_vector)==x.shape[1]
+    diff = x- targe_vector
+    sq_diff = diff **2
+    sq_distance = sq_diff.sum(axis=1)
+    distances = sq_distance ** 0.5
+    # distances = distances.reshape(-1, 1)
+    sorted_distances = distances.argsort()
+    voteIlabel = y[sorted_distances[1]]
     pass
+
+
+if __name__ == "__main__":
+    data_url = "machinelearninginaction/Ch02/datingTestSet.txt"
+    arr_x, arr_y = load_data(data_url)
+    target = [13438,9.665618,0.261333]
+    classify1(target, 1, arr_x, arr_y)
--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 from kNN import createDataSet
 import numpy as np
 import matplotlib.pyplot as plt
+from concurrent import futures
 
 def figure_show(array):
-    assert array.shape[1]==2
+    assert array.shape[1] == 2
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(arr_random[:, 0], arr_random[:, 1])
+    ax.scatter(array[:, 0], array[:, 1])
     plt.show()
 
 
+def rescaling(arr_random):
+    """
+    数据归一化
+    :param arr_random:
+    :return:
+    """
+    min_value = arr_random.min(0)
+    max_value = arr_random.max(0)
+    result_rescaling = (arr_random - min_value) / (max_value - min_value)
+    return result_rescaling
+
+
+def normalization(target_arr):
+    """
+    数据标准化
+    :param target_arr:
+    :return:
+    """
+    mean_value = arr_random.mean(0)
+    min_value = arr_random.min(0)
+    max_value = arr_random.max(0)
+    result_normalization = (arr_random - mean_value) / (max_value - min_value)
+    return result_normalization
+
+
 if __name__ == '__main__':
-    arr_random = np.random.randn(200, 2)
-    figure_show(arr_random)
-    # 准备对arr_random 归一化处理： 取最大值、最小值，对每个值做运算；
+    arr_random = np.random.randn(100, 2)
+    arr_rescaling = rescaling(arr_random)
+    arr_normalization = normalization(arr_random)
 
-
+    with futures.ProcessPoolExecutor(4) as executor:
+        executor.submit(figure_show,arr_random)
+        executor.submit(figure_show, arr_rescaling)
+        executor.submit(figure_show, arr_normalization)
+    pass
